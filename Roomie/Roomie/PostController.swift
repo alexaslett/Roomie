9eb845be7959d22a0e333eefae4a5252cafb22ae
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MacBook-Pro.local>
2017-09-20 10:37:16 -0600

    finish post storyboard and create post controller

---
+++ Roomie/Roomie/PostController.swift
@@ -0,0 +1,10 @@
+//
+//  PostController.swift
+//  Roomie
+//
+//  Created by Kaitlyn Barker on 9/20/17.
+//  Copyright © 2017 One Round Technology. All rights reserved.
+//
+
+import Foundation
+import CloudKit
