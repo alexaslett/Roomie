d5701483f6d66e71807870ee9dc3a9b4fe71131c
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MacBook-Pro.local>
2017-09-20 09:21:11 -0600

    add group property to task model object

--- Roomie/Roomie/Task.swift
+++ Roomie/Roomie/Task.swift
@@ -14,28 +14,33 @@ class Task {
     private static var ownerKey: String { return "owner"}
     private static var isCompleteKey: String { return "isComplete" }
     private static var dueDateKey: String { return "dueDate" }
+    private static var groupKey: String { return "group" }
     static var recordType: String { return "Task" }
     
     let owner: CKReference
     let isComplete: Bool
     let dueDate: Date
+    let group: CKReference
     
     var ckRecordID: CKRecordID?
     
-    init(owner: CKReference, isComplete: Bool, dueDate: Date = Date()) {
+    init(owner: CKReference, isComplete: Bool, dueDate: Date = Date(), group: CKReference) {
         self.owner = owner
         self.isComplete = isComplete
         self.dueDate = dueDate
+        self.group = group
     }
     
     init?(ckRecord: CKRecord) {
         guard let owner = ckRecord[Task.ownerKey] as? CKReference,
-        let isComplete = ckRecord[Task.isCompleteKey] as? Bool,
-            let dueDate = ckRecord[Task.dueDateKey] as? Date else { return nil }
+            let isComplete = ckRecord[Task.isCompleteKey] as? Bool,
+            let dueDate = ckRecord[Task.dueDateKey] as? Date,
+            let group = ckRecord[Task.groupKey] as? CKReference else { return nil }
         
         self.owner = owner
         self.isComplete = isComplete
         self.dueDate = dueDate
+        self.group = group
         self.ckRecordID = ckRecord.recordID
     }
     
@@ -44,6 +49,7 @@ class Task {
         record.setValue(self.owner, forKey: Task.ownerKey)
         record.setValue(self.isComplete, forKey: Task.isCompleteKey)
         record.setValue(self.dueDate, forKey: Task.dueDateKey)
+        record.setValue(self.group, forKey: Task.groupKey)
         
         return record
     }
@@ -51,6 +57,6 @@ class Task {
 
 extension Task: Equatable {
     static func ==(lhs: Task, rhs: Task) -> Bool {
-        return lhs.owner == rhs.owner && lhs.isComplete == rhs.isComplete && lhs.dueDate == rhs.dueDate && lhs.ckRecordID == rhs.ckRecordID
+        return lhs.owner == rhs.owner && lhs.isComplete == rhs.isComplete && lhs.dueDate == rhs.dueDate && lhs.group == rhs.group && lhs.ckRecordID == rhs.ckRecordID
     }
 }
