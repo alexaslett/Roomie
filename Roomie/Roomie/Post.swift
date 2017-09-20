a89e01b94add07a69d4d5472c3d3bf99a09cefda
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MacBook-Pro.local>
2017-09-20 09:44:15 -0600

    start working on post storyboard and model object

---
+++ Roomie/Roomie/Post.swift
@@ -0,0 +1,10 @@
+//
+//  Post.swift
+//  Roomie
+//
+//  Created by Kaitlyn Barker on 9/20/17.
+//  Copyright © 2017 One Round Technology. All rights reserved.
+//
+
+import Foundation
+import CloudKit
