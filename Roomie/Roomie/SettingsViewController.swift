bac9fe6506b6db25305eaf8cfef4ef0557283cc9
macbook pro <macbookpro@slkc.firstdigital.com>
2017-09-24 21:44:34 -0600

    Setting's Storyboard and add view controller

---
+++ Roomie/Roomie/SettingsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsViewController.swift
+//  Roomie
+//
+//  Created by macbook pro on 9/24/17.
+//  Copyright © 2017 One Round Technology. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var profileButton: UIButton!
+    @IBOutlet weak var contactButton: UIButton!
+    @IBOutlet weak var rateButton: UIButton!
+    @IBOutlet weak var aboutButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
