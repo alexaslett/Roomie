e69af3389300fc63bd4283cd1f8a0068fe0b3dca
macbook pro <macbookpro@macbooks-MacBook-Pro.local>
2017-09-27 13:41:56 -0600

    Dismiss feature to go back to group screen

---
+++ Roomie/Roomie/MemberCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemberCollectionViewCell.swift
+//  Roomie
+//
+//  Created by macbook pro on 9/27/17.
+//  Copyright © 2017 One Round Technology. All rights reserved.
+//
+
+import UIKit
+
+class MemberCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}
