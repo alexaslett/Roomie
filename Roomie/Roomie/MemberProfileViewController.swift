653992aa71f04e45988c8ff6640c8aabad8ec64a
macbook pro <macbookpro@macbooks-MacBook-Pro.local>
2017-09-28 10:04:03 -0600

    Fixing constraints on expense storyboard an settings

---
+++ Roomie/Roomie/MemberProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MemberProfileViewController.swift
+//  Roomie
+//
+//  Created by macbook pro on 9/27/17.
+//  Copyright © 2017 One Round Technology. All rights reserved.
+//
+
+import UIKit
+
+class MemberProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       self.view?.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+    }
+
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
