712884bb3cec83c08af0699ba3014a5db6df41bc
macbook pro <macbookpro@macbooks-MacBook-Pro.local>
2017-09-28 16:08:10 -0600

    UI members, Ready to merge

--- Roomie/Roomie/MemberProfileViewController.swift
+++ Roomie/Roomie/MemberProfileViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class MemberProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileView: UIView!
+    
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var phoneNumber: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //profileView.gradientBackGround(colorOne: .blue, colorTwo: .purple)
 
-       self.view?.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        self.view?.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        
+        
     }
 
     @IBAction func cancelButtonTapped(_ sender: Any) {
